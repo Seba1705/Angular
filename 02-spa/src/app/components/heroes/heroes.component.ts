61db868c9725bf1b03d9019c650513a66480a24a
Sebastian1705 <sebaaguirre2012@gmail.com>
2019-07-21 13:13:31 -0300

    Spa terminado

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-heroes',
-    templateUrl: './heroes.component.html',
-    styles: []
+    templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+    heroes:Heroe[] = [];
 
-  ngOnInit() {
-  }
+    constructor( private _heroesService:HeroesService,
+                 private router:Router ) { }
+  
+    ngOnInit() {
+        this.heroes = this._heroesService.getHeroes();
+    }
 
+    verHeroe(idx:number) {
+        this.router.navigate([ '/heroe', idx ]);
+    }
 }
