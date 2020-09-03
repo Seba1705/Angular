829d2548fde34b47880e6fb52ba55af64e0aba8d
Seba1705 <sebaaguirre2012@gmail.com>
2020-09-02 23:29:17 -0300

    Avanzando con las practicas

--- 10-HeroesApp/src/app/app.module.ts
+++ 10-HeroesApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +9,18 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroeComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroeComponent,
+        HeroesComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 10-HeroesApp/src/app/pages/heroe/heroe.component.ts
+++ 10-HeroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from '../../models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { HeroesService } from '../../services/hereos.service';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html',
+    styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+    heroe = new HeroeModel();
 
-  ngOnInit(): void {
-  }
+    constructor(private heroesServices: HeroesService) { }
 
+    ngOnInit(): void {
+    }
+
+    guardar(form: NgForm) {
+        if( form.invalid )
+            return;
+
+        Swal.fire({
+            title: 'Espere',
+            text: 'Guardando información',
+            allowOutsideClick: false,
+            icon: 'info'
+        });
+        Swal.showLoading();
+        
+        let peticion: Observable<any>;
+
+        if(this.heroe.id) 
+            peticion = this.heroesServices.actualizarHeroe(this.heroe);
+        else
+            peticion = this.heroesServices.crearHeroe(this.heroe);
+              
+        peticion.subscribe(resp => {
+            Swal.fire({
+                title: this.heroe.nombre,
+                text: 'Se actulizó correctamente',
+                allowOutsideClick: false,
+                icon: 'success'
+            });
+
+            console.log(resp);
+        })
+
+        
+    }
 }
